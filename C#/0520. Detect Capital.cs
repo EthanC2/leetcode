af547066a146cfa6c1beaa9a8ef9dd0e35302c3d
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-01-24 10:18:25 -0500

    Update 0520. Detect Capital.cs

--- C#/0520. Detect Capital.cs
+++ C#/0520. Detect Capital.cs
@@ -1,5 +1,10 @@
 //Optimal Solution: https://leetcode.com/problems/detect-capital/discuss/1715652/C-Easy-understanding-2-line-solution
 
+/*
+    The brilliance of the optimal solution above is that it only requires you to traverse the string once, 
+    making its time complexity O(1N). Furthermore, it doesn't have to allocate a new string like mine did.
+*/
+
 public class Solution 
 {
     public bool DetectCapitalUse(string word) 
