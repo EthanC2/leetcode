a420c6348ba946109d86a839fb677896cd3fb470
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-01-25 21:29:12 -0500

    Update 0824. Goat Latin.cs

--- C#/0824. Goat Latin.cs
+++ C#/0824. Goat Latin.cs
@@ -1,1 +1,30 @@
-
+public class Solution {
+    public string ToGoatLatin(string sentence) {
+        string[] words = sentence.Split(' ');
+        StringBuilder AsGoatLatin = new();
+        int count = 0;
+        
+        foreach(var word in words)
+        {
+            count += 1;
+            
+            if (IsVowel(word[0]))
+                AsGoatLatin.Append($"{word}ma");
+            else
+                AsGoatLatin.Append($"{word[1..]}{word[0]}ma");
+            
+            AsGoatLatin.Append('a', count);
+            if (count < words.Length)
+                AsGoatLatin.Append(' ');
+        }
+        
+        return AsGoatLatin.ToString();
+    }
+    
+    public bool IsVowel(char c)
+    {
+        var Vowels = new HashSet<char>() {'a', 'e', 'i', 'o', 'u'};
+            
+        return Vowels.Contains(Char.ToLower(c));
+    }
+}
