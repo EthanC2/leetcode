431443195059c50f4f97adc92b036aa0291dffe6
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-01 08:20:00 -0500

    Update 0121. Best Time to Buy and Sell Stock.cs

--- C#/0121. Best Time to Buy and Sell Stock.cs
+++ C#/0121. Best Time to Buy and Sell Stock.cs
@@ -1,4 +1,8 @@
-// Naive approach
+/*
+    Naive approach
+    TC: O(n^2)
+    SC: O(1)
+*/
 public class Solution 
 {
     public int MaxProfit(int[] prices) 
@@ -21,4 +25,26 @@ public class Solution
 
 
 
-// Optimized approach
+/*
+    Optimized solution:
+    TC: O(n)
+    SC: O(1)
+*/
+public class Solution 
+{
+    public int MaxProfit(int[] prices) 
+    {
+        int minPrice = Int32.MaxValue;
+        int maxProfit = 0;
+        
+        for(int i=0; i < prices.Length; i++)
+        {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+            else if (prices[i] - minPrice > maxProfit)
+                maxProfit = prices[i] - minPrice;
+        }
+        
+        return maxProfit;
+    }
+}
