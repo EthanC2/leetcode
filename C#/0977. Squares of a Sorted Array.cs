06124ab64676d26f28dff336d05a09ac2cd36c1b
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-07 16:33:03 -0500

    Update 0977. Squares of a Sorted Array.cs

--- C#/0977. Squares of a Sorted Array.cs
+++ C#/0977. Squares of a Sorted Array.cs
@@ -17,3 +17,35 @@ public class Solution
       return nums;
     }
 }
+
+
+/*
+    Optimized approach: two pointers
+    
+    TC: O(n)
+    SC: O(1)
+*/
+public class Solution 
+{
+    public int[] SortedSquares(int[] nums) 
+    {
+      int[] squares = new int[nums.Length];
+      int left = 0, right = nums.Length-1;
+      
+      for(int i=nums.Length-1; i >= 0; --i)
+      {
+        if (Math.Abs(nums[left]) < Math.Abs(nums[right]))
+        {
+          squares[i] = nums[right]*nums[right];
+          --right;
+        }
+        else
+        {
+          squares[i] = nums[left]*nums[left];
+          ++left;
+        }
+      }
+      
+      return squares;
+    }
+}
