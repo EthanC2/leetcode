e55e2be63f9f92056d7ab4513598755de8cb72bb
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-07 10:19:02 -0500

    Update 0389. Find the Difference.cs

--- C#/0389. Find the Difference.cs
+++ C#/0389. Find the Difference.cs
@@ -1,9 +1,45 @@
+/*
+    Naive approach: Sorting
+    TC: O(n log n) | sorting
+    SC: O(n)       | iterating char by char
+*/
 public class Solution 
 {
     public char FindTheDifference(string s, string t) 
     {
-      Dictionary<char,int> smap = new(), tmap = new();
+      //assumption: s.Length+1 == t.Length
+      var ssorted = s.ToCharArray();
+      var tsorted = t.ToCharArray();
+    
+      //Sort the arrays
+      Array.Sort(ssorted);
+      Array.Sort(tsorted);
       
+      //Compare them letter by letter
+      for(int i = 0; i < s.Length; ++i)
+      {
+        if (ssorted[i] != tsorted[i])
+          return tsorted[i];
+      }
+      
+      //Else it's the last character
+      return tsorted[t.Length-1];
+    }
+}
+
+
+/*
+    Optimized approach: hashmap
+    TC: O(n) | iterating over the whole string
+    SC: O(n) | for storing each char in the string 
+*/
+public class Solution 
+{
+    public char FindTheDifference(string s, string t) 
+    {
+      Dictionary<char,int> smap = new();
+      
+      //Create a reference hashmap for string s
       foreach(char c in s)
       {
         if (smap.ContainsKey(c))
@@ -12,17 +48,16 @@ public class Solution
           smap.Add(c,1);
       }
       
+      //Compare string t to hashmap of s
       foreach(char c in t)
       {
-        if (tmap.ContainsKey(c))
-          tmap[c]++;
-        else
-          tmap.Add(c,1);
-        
-        if (!smap.ContainsKey(c) || smap[c] < tmap[c])
+        if (!smap.ContainsKey(c) || smap[c] == 0)
           return c;
+      
+        smap[c]--;
       }
       
-      return '0';
+      //Should never happen
+      return '\0';
     }
 }
