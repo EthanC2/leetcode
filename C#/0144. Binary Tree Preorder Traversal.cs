c971120377c1381b217cc59e8ffd7ccb838e749d
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-08 12:05:52 -0500

    Update 0144. Binary Tree Preorder Traversal.cs

--- C#/0144. Binary Tree Preorder Traversal.cs
+++ C#/0144. Binary Tree Preorder Traversal.cs
@@ -1,5 +1,13 @@
 /*
-
+    What is preorder traversal of a binary search tree?:
+    https://www.javatpoint.com/preorder-traversal
+    https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+    
+    Preorder Traversal
+    =================-
+    list.Add(root.val);
+    PreOrder(root.left);
+    PreOrder(root.right);
 */
 
 //Recursive
