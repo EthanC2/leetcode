e5c5f931adfec842597bddcb6c5997a0a3e2187b
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-08 11:41:06 -0500

    Update 0094. Binary Tree Inorder Traversal.cs

--- C#/0094. Binary Tree Inorder Traversal.cs
+++ C#/0094. Binary Tree Inorder Traversal.cs
@@ -23,19 +23,6 @@ public class Solution
 
 
 //Iterative approach
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution 
 {
     public IList<int> InorderTraversal(TreeNode root) 
