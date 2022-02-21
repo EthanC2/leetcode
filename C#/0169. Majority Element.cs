14579c2c5e2002e1311a102bb7f658b6570d3c92
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-21 17:02:19 -0500

    Create 0169. Majority Element.cs

---
+++ C#/0169. Majority Element.cs
@@ -0,0 +1,25 @@
+/*
+  Native Approach: Brute Force
+  TC: O(n)
+  SC: O(n)
+*/
+public class Solution 
+{
+    public int MajorityElement(int[] nums) 
+    {
+      Dictionary<int,int> map = new();
+      
+      //Count the amount of each number in the array
+      foreach(int num in nums)
+      {
+        if (map.ContainsKey(num)) map[num]++;
+        else map.Add(num,1);
+      
+        //If an element appears more than N/2+1 times, it's the majority
+        if (map[num] >= nums.Length/2+1)
+          return num;
+      }
+      
+      return nums[0];  //make the compiler happy
+    }
+}
