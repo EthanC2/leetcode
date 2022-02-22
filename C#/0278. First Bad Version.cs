b4a696f74a6fbf6497a37a05a2d0a9bb1e4dae42
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-21 23:03:33 -0500

    Update 0278. First Bad Version.cs

--- C#/0278. First Bad Version.cs
+++ C#/0278. First Bad Version.cs
@@ -6,7 +6,11 @@
     O(log n) Solution
     =================
     Since n is the most recent version, we can interpret
-    n as a series of version from 1..n.
+    n as a series of versions from 1..n. If you encounter a 
+    bad version 'x', you can't guarantee that x-1 is not a bad
+    version without calling a 'IsBadVersion(x-1)', so the best
+    way to deal with that is just to include x in the dataset,
+    changing the normal 'high = mid-1' to 'high = mid'.
 */
 public class Solution : VersionControl 
 {
