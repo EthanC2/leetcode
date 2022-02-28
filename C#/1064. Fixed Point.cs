3913db9dcfa5872f79296b2b07bdb58ba1385a20
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-02-28 13:48:06 -0500

    Update 1064. Fixed Point.cs

--- C#/1064. Fixed Point.cs
+++ C#/1064. Fixed Point.cs
@@ -7,19 +7,13 @@ public class Solution
 {
     public int FixedPoint(int[] arr) 
     {
-      int min = -1;
-      bool found = false;
-      
-      for(int i=0; i < arr.Length; ++i)
+     for(int i=0; i < arr.Length; ++i)
       {
-        if (i == arr[i] && !found)
-        {
-          min = i;
-          found = true;
-        }
+        if (i == arr[i])
+          return i;
       }
       
-      return min;
+      return -1;
     }
 }
 
