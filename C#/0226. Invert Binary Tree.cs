fdbb77092d0dfb322497191864278f015fb802a3
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-03-02 00:48:53 -0500

    Create 0226. Invert Binary Tree.cs

---
+++ C#/0226. Invert Binary Tree.cs
@@ -0,0 +1,56 @@
+
+
+/*
+  Recursive Solution
+  TC: O(n)
+  SC: O(n)
+  
+  Since you have to return 'root', you have to write a helper function
+*/
+public class Solution 
+{
+    public TreeNode InvertTree(TreeNode root) 
+    {
+        InvertTreeHelper(root);
+        return root;
+    }
+    
+    public void InvertTreeHelper(TreeNode node)
+    {
+        if (node is null) return;
+        
+        (node.left, node.right) = (node.right, node.left);
+        
+        InvertTree(node.left);
+        InvertTree(node.right);
+    }
+}
+
+
+/*
+  Iterative Solution
+  TC: O(n)
+  SC: O(n)
+  
+  Just a bit cleaner due to the lack of the helper function
+*/
+public class Solution 
+{
+    public TreeNode InvertTree(TreeNode root) 
+    {
+        if (root is null) return null;
+        Queue<TreeNode> nodes = new();
+        nodes.Enqueue(root);
+        
+        while (nodes.Count != 0)
+        {
+            TreeNode curr = nodes.Dequeue();
+            (curr.left, curr.right) = (curr.right, curr.left);
+            
+            if (curr.left is not null) nodes.Enqueue(curr.left);
+            if (curr.right is not null) nodes.Enqueue(curr.right);
+        }
+        
+        return root;
+    }
+}
