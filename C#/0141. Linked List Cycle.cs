123114d5fb48ee5e224a47d2d68a4e7fb5bae907
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-03-08 10:23:53 -0500

    Update 0141. Linked List Cycle.cs

--- C#/0141. Linked List Cycle.cs
+++ C#/0141. Linked List Cycle.cs
@@ -1,7 +1,9 @@
 /*
-  Approach: use a hashset on this ListNode objects themselves instead 
-            of their values. If you encounter the same object in memory,
-            then you already visited it
+  Naive Approach: 
+  ==============
+  use a hashset on this ListNode objects themselves instead 
+  of their values. If you encounter the same object in memory,
+  then you already visited it
 
   TC: O(n)
   SC: O(n)
@@ -23,5 +25,32 @@ public class Solution
         }
         
         return false;
+    }
+}
+
+
+/*
+  Optimized Approach: Floyd's Cycle Detection Algorithm (Tortoise and Harse):
+  ===========================================================================
+  Use a fast and slow pointer. If there is a cycle, the two pointers will necessarily
+  overlap at some point. If either pointer reaches null, then there is no cycle.
+*/
+public class Solution 
+{
+    public bool HasCycle(ListNode head) 
+    {
+        if (head is null) return false;
+        ListNode slow = head, fast = head.next;
+        
+        while (fast != slow)
+        {
+            if (fast is null || fast.next is null)
+                return false;
+                
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return true;
     }
 }
