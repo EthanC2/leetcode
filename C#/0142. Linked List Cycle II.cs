693c9b746eb7eefca756d98bdd8699f179615540
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-03-08 11:54:59 -0500

    Update 0142. Linked List Cycle II.cs

--- C#/0142. Linked List Cycle II.cs
+++ C#/0142. Linked List Cycle II.cs
@@ -26,3 +26,52 @@ public class Solution
         return null;
     }
 }
+
+/*
+  Optimized Approach: Floyd's Cycle Detection Algorithm (Tortoise and Harse):
+  ===========================================================================
+  Use a fast and slow pointer. If there is a cycle, the two pointers will necessarily
+  overlap at some point. If either pointer reaches null, then there is no cycle.
+  After finding the intersection, set one pointer to the beginning of the linked list
+  and another to the intersection point. From there, iterate until the two converge.
+  The point where the two converge will always be the beginning of the cycle.
+  
+  TC: O(n)
+  SC: O(1)
+*/
+public class Solution 
+{
+    public ListNode DetectCycle(ListNode head) 
+    {
+        if (head is null)
+            return null;
+        
+        ListNode intersection = FindIntersection(head);
+        if (intersection is null)
+            return null;
+    
+        while (head != intersection)
+        {
+            head = head.next;
+            intersection = intersection.next;
+        }
+        
+        return head;  //head == intersection
+    }
+    
+    public ListNode FindIntersection(ListNode head)
+    {
+        ListNode slow = head, fast = head;
+        
+        while (fast is not null && fast.next is not null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast)
+                return slow;
+        }
+        
+        return null;
+    }
+}
