60017a3a22a759d1958baa52e4ee0a8c87dd9b0e
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-03-13 21:43:12 -0400

    Create 0071. Simplify Path.cs

---
+++ C#/0071. Simplify Path.cs
@@ -0,0 +1,42 @@
+/*
+    TC: O(n) for iterating over the entire list
+    SC: O(n) 
+*/
+public class Solution 
+{
+    public string SimplifyPath(string path) 
+    {
+        LinkedList<string> deque = new();
+        string[] dirs = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        
+        foreach(string dir in dirs)
+        {
+            switch (dir)
+            {
+                case ".":
+                    continue;
+                
+                case "..":
+                    if (deque.Count != 0)
+                    {
+                        deque.RemoveLast();
+                    }
+                    break;
+                    
+                default:
+                    deque.AddLast(dir);
+                    break;
+            }
+        }
+        
+        //Join results
+        StringBuilder result = new();
+        while (deque.Count != 0)
+        {
+            result.Append("/" + deque.First.Value);
+            deque.RemoveFirst();
+        }
+        
+        return result.Length == 0 ? "/" : result.ToString();
+    }
+}
