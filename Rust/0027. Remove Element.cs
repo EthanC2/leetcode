cfb70402c15c02f0e34e058c8cca89f150e8e6e7
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-03-14 19:58:27 -0400

    Create 0027. Remove Element.cs

---
+++ Rust/0027. Remove Element.cs
@@ -0,0 +1,18 @@
+/*
+  TC: O(n)
+  SC: O(1)
+*/
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut idx = 0;
+        
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums[idx] = nums[i];
+                idx += 1;
+            }
+        }
+        
+        idx as i32
+    }
+}
