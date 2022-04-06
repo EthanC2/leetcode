0829d89e8b948bbdf3401b9a2edfcee65110ffee
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-04-06 16:59:36 -0700

    Create 0211. Design Add and Search Words Data Structure.rs

---
+++ Rust/0211. Design Add and Search Words Data Structure.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+
+struct TrieNode {
+    is_end: bool,
+    children: HashMap<char,TrieNode>,    
+}
+
+struct WordDictionary {
+    root: TrieNode,
+}
+
+
+impl TrieNode {
+    fn new() -> TrieNode {
+        TrieNode {is_end: false, children: HashMap::new()}
+    }
+}
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl WordDictionary {
+
+    fn new() -> Self {
+        WordDictionary{root: TrieNode::new()}
+    }
+    
+    fn add_word(&mut self, word: String) {
+        let mut current_node = &mut self.root;
+        for c in word.chars() {
+            if !current_node.children.contains_key(&c) {
+                current_node.children.insert(c, TrieNode::new());
+            }
+                
+            current_node = current_node.children.get_mut(&c).unwrap();
+        }
+        
+        current_node.is_end = true;
+    }
+    
+    fn search(&self, word: String) -> bool {
+        self.internal_search(word.as_str(), &self.root)
+    }
+    
+    fn internal_search(&self, substr: &str, start: &TrieNode) -> bool {
+        let mut current_node = start;
+        for (i,c) in substr.chars().enumerate() {                      
+            match c {
+                '.' => return current_node.children.values().into_iter().any(|child| self.internal_search(&substr[(i+1)..], &child)),
+                 _ => match current_node.children.get(&c) {
+                    Some(child) => current_node = child,
+                    None => return false,
+                },
+            }
+        }
+        
+        current_node.is_end
+    }
+}
